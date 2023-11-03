ba625f63c8db9cea96ef26eaed4432c2228824a9
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-11-03 09:09:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,21 +18,21 @@ function calculateMinCost() {
   
 let total=0;
 arr=arr.sort(compare);
-
+// console.log("arr ",arr);
 while(arr.length>=2){
   let sum=arr[0]+arr[1];
-
+  // console.log(sum);
   let rem=[];
   rem.push(sum);
   for(let k=2;k<arr.length;k++){
     rem.push(arr[k]);
   }
   rem=rem.sort(compare);
-
+  // console.log("rem ",rem);
   arr=[...rem];
-
+  // console.log("arr ",arr);
   total=total+sum;
- 
+  // console.log("total "+total+"----------------------");
 }
 console.log(total);
  
